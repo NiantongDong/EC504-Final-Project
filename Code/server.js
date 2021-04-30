f431895a9b703037fd9a73c94dd93599ac1270a8
NiantongDong <niantongdong@gmail.com>
2021-04-29 22:45:30 -0400

    Add Sorting Visualizer

--- Code/server.js
+++ Code/server.js
@@ -6,7 +6,12 @@ app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 })
-
+app.get('/Pathfinding.html', function(req, res){
+  res.sendFile(__dirname + "/Pathfinding.html");
+});
+app.get('/Sorting.html', function(req, res){
+  res.sendFile(__dirname + "/Sorting.html");
+});
 app.listen(3000, () => {
   console.log("The server is up and running!");
 });
